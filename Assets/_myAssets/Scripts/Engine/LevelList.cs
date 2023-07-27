4f0f66913c067252685434d64b70da604b277a6a
KubilayDekin <kubilaydekin@gmail.com>
2023-07-28 00:20:06 +0300

    scene management system

---
+++ Assets/_myAssets/Scripts/Engine/LevelList.cs
@@ -0,0 +1,14 @@
+﻿using Assets._myAssets.Scripts.Extensions;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._myAssets.Scripts.Engine
+{
+	[Serializable]
+	[CreateAssetMenu(fileName = "LevelList", menuName = "Data/Level List")]
+	public class LevelList : ScriptableObject
+	{
+		public List<SceneObject> levels;
+	}
+}
---
+++ Assets/_myAssets/Scripts/Engine/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using Assets._myAssets.Scripts.Gameplay;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets._myAssets.Scripts.Engine
+{
+	public class LevelManager : SingletonMonoBehaviour<LevelManager>
+	{
+		public LevelList levelList;
+		private int currentLevelIndex = 0;
+
+		void Start()
+		{
+			LoadCurrentLevel();
+		}
+
+		public void LoadNextLevel()
+		{
+			currentLevelIndex++;
+			if (currentLevelIndex >= levelList.levels.Count)
+			{
+				return;
+			}
+
+			LoadCurrentLevel();
+		}
+
+		public void ReloadLevel()
+		{
+			LoadCurrentLevel();
+		}
+
+		private void LoadCurrentLevel()
+		{
+			SceneManager.LoadSceneAsync(levelList.levels[currentLevelIndex].SceneName);
+		}
+	}
+}
