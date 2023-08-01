3570bcd829e6b845b80c80d4d30ef5d7cf1d9ab0
KubilayDekin <kubilaydekin@gmail.com>
2023-08-02 00:45:38 +0300

    missing references

--- Assets/_myAssets/Scripts/LevelDesign/LevelAttributes.cs
+++ Assets/_myAssets/Scripts/LevelDesign/LevelAttributes.cs
@@ -5,6 +5,7 @@ namespace Assets._myAssets.Scripts.LevelDesign
 	[CreateAssetMenu(fileName = "LevelAttributes", menuName = "LevelDesign/Level Attributes")]
 	public class LevelAttributes : ScriptableObject
 	{
+		[Range(1,100)]
 		public int BallSpawnCount;
 		public int TargetBallCount;
 	}
